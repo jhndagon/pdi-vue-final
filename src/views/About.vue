c67cd67589edf2dff62f4d008c37bcffc27d4d17
Luis Monsalve <ernesto.monsalve@udea.edu.co>
2020-12-12 21:54:01 -0500

    Acerca de

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Procesamiento digital de imágenes de algas</h1>
+    <p class="my-5">
+      Este es un proyecto presentado para el curso de procesamiento digital de imágenes
+      dictado por la facultad de Ingeniería de la Universidad de Antioquia
+    </p>
+
+    <b-row class="py-5">
+      <b-col sm="6">
+        <b>Luis Ernesto Monsalve Arango</b><br />ernesto.monsalve@udea.edu.co</b-col
+      >
+      <b-col sm="6">
+        <b>John David González Alzate</b><br />johnd.gonzalez@udea.edu.co
+      </b-col>
+    </b-row>
   </div>
 </template>
